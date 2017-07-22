451041546569b91a976352a3bc05c50f3aeb1f79
Robert Zobrist <zobrist.robert.480@gmail.com>
2017-07-22 10:05:07 -0700

    added heroky as remote repository

--- hbb.js
+++ hbb.js
@@ -11,27 +11,27 @@ var handlebars = require('express-handlebars');
 // and documentation.
 
 keystone.init({
-	'name': 'HumanBrainBox',
-	'brand': 'HumanBrainBox',
+    'name': 'HumanBrainBox',
+    'brand': 'HumanBrainBox',
 
-	'less': 'public',
-	'static': 'public',
-	'favicon': 'public/favicon.ico',
-	'views': 'templates/views',
-	'view engine': '.hbs',
+    'less': 'public',
+    'static': 'public',
+    'favicon': 'public/favicon.ico',
+    'views': 'templates/views',
+    'view engine': '.hbs',
 
-	'custom engine': handlebars.create({
-		layoutsDir: 'templates/views/layouts',
-		partialsDir: 'templates/views/partials',
-		defaultLayout: 'default',
-		helpers: new require('./templates/views/helpers')(),
-		extname: '.hbs',
-	}).engine,
+    'custom engine': handlebars.create({
+        layoutsDir: 'templates/views/layouts',
+        partialsDir: 'templates/views/partials',
+        defaultLayout: 'default',
+        helpers: new require('./templates/views/helpers')(),
+        extname: '.hbs',
+    }).engine,
 
-	'auto update': true,
-	'session': true,
-	'auth': true,
-	'user model': 'User',
+    'auto update': true,
+    'session': true,
+    'auth': true,
+    'user model': 'User',
 });
 
 // Load your project's Models
@@ -41,10 +41,10 @@ keystone.import('models');
 // bundled templates and layouts. Any runtime locals (that should be set uniquely
 // for each request) should be added to ./routes/middleware.js
 keystone.set('locals', {
-	_: require('lodash'),
-	env: keystone.get('env'),
-	utils: keystone.utils,
-	editable: keystone.content.editable,
+    _: require('lodash'),
+    env: keystone.get('env'),
+    utils: keystone.utils,
+    editable: keystone.content.editable,
 });
 
 // Load your project's Routes
@@ -53,14 +53,12 @@ keystone.set('routes', require('./routes'));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
-	posts: ['posts', 'post-categories'],
-	galleries: 'galleries',
-	enquiries: 'enquiries',
-	users: 'users',
+    posts: ['posts', 'post-categories'],
+    galleries: 'galleries',
+    enquiries: 'enquiries',
+    users: 'users',
 });
 
 // Start Keystone to connect to your database and initialise the web server
-
-
 
 keystone.start();
