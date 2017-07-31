96ee55fddeae7d6648586cea7dd313fdc57ca3a1
Robert Zobrist <zobrist.robert.480@gmail.com>
2017-07-30 17:26:10 -0700

    major progress on home page(see benchmarks.doc)

--- models/Forms.js
+++ models/Forms.js
@@ -7,14 +7,14 @@ var Types = keystone.Field.Types;
  */
 
 var Forms = new keystone.List('Forms', {
-	autokey: { from: 'name', path: 'key', unique: true },
+    autokey: { from: 'name', path: 'key', unique: true },
 });
 
 Forms.add({
-	name: { type: String, required: true },
-	publishedDate: { type: Date, default: Date.now },
-	heroImage: { type: Types.CloudinaryImage },
-	images: { type: Types.CloudinaryImages },
+    name: { type: String, required: true },
+    publishedDate: { type: Date, default: Date.now },
+    heroImage: { type: Types.CloudinaryImage },
+    images: { type: Types.CloudinaryImages },
 });
 
 Forms.register();
--- models/FreeTrial.js
+++ models/FreeTrial.js
@@ -2,14 +2,14 @@ const keystone = require('keystone');
 const Types = keystone.Field.Types;
 
 const FreeTrial = new keystone.List('FreeTrial', {
-	
-	autokey: {path: 'slug', from: 'title', unique: true}
+
+    autokey: { path: 'slug', from: 'title', unique: true },
 });
 FreeTrial.add({
-	title: {type: String, required: false},
-	image: {type: Types.CloudinaryImage},
-	description: {type: Types.Html, wysiwyg: true, height: 300},
-	publishedDate: {type: Date, default: Date.now}
+    title: { type: String, required: false },
+    image: { type: Types.CloudinaryImage },
+    description: { type: Types.Html, wysiwyg: true, height: 300 },
+    publishedDate: { type: Date, default: Date.now },
 });
 
 FreeTrial.register();
